797bc4b1e46b54241f7fe489c021c1b4bb11c86d
Jorge Perez <jp10791@hotmail.com>
2022-08-22 11:00:19 -0400

    init new project file

---
+++ T Rex Endless Runner/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace T_Rex_Endless_Runner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trex = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.pictureBox1.Location = new System.Drawing.Point(-5, 410);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(971, 56);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trex
+            // 
+            this.trex.Image = global::T_Rex_Endless_Runner.Properties.Resources.running;
+            this.trex.Location = new System.Drawing.Point(108, 367);
+            this.trex.Name = "trex";
+            this.trex.Size = new System.Drawing.Size(40, 43);
+            this.trex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.trex.TabIndex = 1;
+            this.trex.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::T_Rex_Endless_Runner.Properties.Resources.obstacle_1;
+            this.pictureBox3.Location = new System.Drawing.Point(562, 364);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(23, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacle";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::T_Rex_Endless_Runner.Properties.Resources.obstacle_2;
+            this.pictureBox4.Location = new System.Drawing.Point(820, 377);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "obstacle";
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(106, 24);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "Score: 0";
+            this.txtScore.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(962, 465);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.trex);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Name = "Form1";
+            this.Tag = "obstacle";
+            this.Text = "T Rex Endless Runner Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox trex;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ T Rex Endless Runner/Sound.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace T_Rex_Endless_Runner
+{
+    public class Sound
+    {
+        private WaveStream _soundplayerBgStream;
+        private WaveOut _soundplayerBgOut;
+        private WaveStream _soundJumpplayerStream;
+        private WaveOut _soundJumpplayerOut;
+        private WaveStream _soundDeathplayerStream;
+        private WaveOut _soundDeathplayerOut;
+
+        public Sound()
+        {
+            var soundBgFile = System.IO.Directory.GetCurrentDirectory() + "\\sound\\runtrex.wav";
+            var soundJumpFile = System.IO.Directory.GetCurrentDirectory() + "\\sound\\jump.wav";
+            var soundDeathFile = System.IO.Directory.GetCurrentDirectory() + "\\sound\\death.wav";
+
+            //initiate Background sound
+            _soundplayerBgStream = new AudioFileReader(soundBgFile);
+            _soundplayerBgOut = new WaveOut();
+            _soundplayerBgOut.Init(_soundplayerBgStream);
+
+            //initiate the shuthug
+            _soundJumpplayerStream = new AudioFileReader(soundJumpFile);
+            _soundJumpplayerOut = new WaveOut();
+            _soundJumpplayerOut.Init(_soundJumpplayerStream);
+
+            //initiate Background sound
+            _soundplayerBgStream = new AudioFileReader(soundBgFile);
+            _soundplayerBgOut = new WaveOut();
+            _soundplayerBgOut.Init(_soundplayerBgStream);
+
+            //initiate the shuthug
+            _soundDeathplayerStream = new AudioFileReader(soundDeathFile);
+            _soundDeathplayerOut = new WaveOut();
+            _soundDeathplayerOut.Init(_soundDeathplayerStream);
+
+            //start the sound
+            _soundplayerBgOut.Play();
+        }
+
+        public void PlayJumpSound()
+        {
+            //if (_soundplayerBgOut.PlaybackState is PlaybackState.Playing) _soundplayerBgOut.Stop();
+            if (_soundJumpplayerOut.PlaybackState is PlaybackState.Playing) _soundJumpplayerOut.Stop();
+
+            //_soundplayerBgStream.CurrentTime = new TimeSpan(0L);
+            _soundJumpplayerStream.CurrentTime = new TimeSpan(0L);
+            _soundJumpplayerOut.Play();
+            //_soundplayerBgOut.Play();
+
+        }
+
+        public void PlayDeathSound()
+        {
+
+            //if (_soundplayerBgOut.PlaybackState is PlaybackState.Playing) _soundplayerBgOut.Stop();
+            if (_soundDeathplayerOut.PlaybackState is PlaybackState.Playing) _soundDeathplayerOut.Stop();
+
+            _soundplayerBgStream.CurrentTime = new TimeSpan(0L);
+            _soundDeathplayerStream.CurrentTime = new TimeSpan(0L);
+            _soundDeathplayerOut.Play();
+            _soundplayerBgOut.Play();
+
+        }
+    }
+
+
+}
